618e49860e0d6b303878964324f14102b8a4270f
NTTGON <trongthanh0099@gmail.com>
2024-06-04 02:57:43 +0700

    update HMAC

---
+++ middlewares/HMAC_SHA3_256.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using std::cout;
+using std::cerr;
+using std::endl;
+
+#include <string>
+using std::string;
+
+#include <cstdlib>
+using std::exit;
+
+#include "cryptopp/osrng.h"
+using CryptoPP::AutoSeededRandomPool;
+
+#include "cryptopp/cryptlib.h"
+using CryptoPP::Exception;
+
+#include "cryptopp/hmac.h"
+using CryptoPP::HMAC;
+
+#include "cryptopp/sha3.h"
+using CryptoPP::SHA3_256;
+
+#include "cryptopp/hex.h"
+using CryptoPP::HexEncoder;
+using CryptoPP::HexDecoder;
+
+#include "cryptopp/filters.h"
+using CryptoPP::StringSink;
+using CryptoPP::ArraySink;
+using CryptoPP::ArraySource;
+using CryptoPP::StringSource;
+using CryptoPP::HashFilter;
+using CryptoPP::HashVerificationFilter;
+
+#include <cryptopp/files.h>
+using CryptoPP::FileSource;
+using CryptoPP::FileSink;
+
+#include "cryptopp/secblock.h"
+using CryptoPP::SecByteBlock;
+
+#include <cryptopp/queue.h>
+using CryptoPP::ByteQueue;
+#ifdef _WIN32
+	#include <windows.h>
+	#endif
+	#include <cstdlib>
+	#include <locale>
+	#include <cctype>
+
+// Hàm XOR với pad
+SecByteBlock xorWithpad(const unsigned char pad, const SecByteBlock key) {
+    SecByteBlock keyP = key;
+    // Thực hiện phép XOR giữa từng byte của khóa với giá trị pad
+    for (size_t i = 0; i < key.size(); ++i) {
+        keyP[i] ^= pad;
+    }
+    return keyP;
+}
+
+// Hàm HMAC-SHA3-256
+void HMAC_SHA3_256(const char* key_content, const char* input_content, char* output_hexmac, size_t output_size) {
+    // Chuyển key sang SecByteBlock
+    SecByteBlock key(reinterpret_cast<const byte*>(key_content), strlen(key_content));
+
+    string mac, i_mac;
+    const unsigned char ipad = 0x36;
+    const unsigned char opad = 0x5c;
+    SecByteBlock k_ipad = xorWithpad(ipad, key);
+    SecByteBlock k_opad = xorWithpad(opad, key);
+
+    string file_content(input_content);
+
+    // Hash (k xor ipad) với nội dung file
+    HMAC<SHA3_256> i_hmac(k_ipad, k_ipad.size());
+    StringSource(file_content, true, 
+        new HashFilter(i_hmac,
+            new StringSink(i_mac)
+        ) 
+    );
+
+    // Hash (k xor opad) với (i_mac)
+    HMAC<SHA3_256> hmac(k_opad, k_opad.size());
+    StringSource(i_mac, true, 
+        new HashFilter(hmac,
+            new StringSink(mac)
+        ) 
+    );
+
+    // Chuyển kết quả sang dạng hex
+    string hexmac;
+    StringSource(mac, true, new HexEncoder(new StringSink(hexmac)));
+
+    // Sao chép kết quả hex vào output_hexmac
+    strncpy(output_hexmac, hexmac.c_str(), output_size);
+}
+
--- middlewares/HMAC_SHA3_256.py
+++ middlewares/HMAC_SHA3_256.py
@@ -11,56 +11,25 @@ lib = ctypes.CDLL(sopath,winmode=ctypes.DEFAULT_MODE)
 
 # Set up the prototype of the function
 # All of then are strings (char*)
-HMAC_SHA3_256 = lib._Z13HMAC_SHA3_256PKcS0_S0_ 
-HMAC_SHA3_256.argtypes = [c_char_p,c_char_p,c_char_p]
+HMAC_SHA3_256 = lib._Z13HMAC_SHA3_256PKcS0_Pcy 
+HMAC_SHA3_256.argtypes = [c_char_p,c_char_p,c_char_p,ctypes.c_size_t]
 HMAC_SHA3_256.restype = None  
 
-GenerateAndSaveKey = lib._Z18GenerateAndSaveKeyPKc 
-GenerateAndSaveKey.argtypes = [c_char_p]
-GenerateAndSaveKey.restype = None  
 
 # Wrapped functions
-def call_HMAC (keyFIle,input_filename,output_filename):
-    # convert python strings to bytes, as ctypes works with bytes
-    keyFIle = keyFIle.encode('utf-8')
-    input_filename = input_filename.encode('utf-8')
-    output_filename = output_filename.encode('utf-8')
+def hmac_sha3_256(key, data):
+    key = key.encode('utf-8')
+    data = data.encode('utf-8')
 
-    # call the c function
-    HMAC_SHA3_256(keyFIle,input_filename,output_filename)
-    print("HMAC-SHA3-256 calculation completed and written to", output_filename.decode('utf-8'))
+    # Chuẩn bị bộ đệm cho kết quả hexmac
+    output_hexmac = ctypes.create_string_buffer(64)  # SHA3-256 sẽ cho ra 32 bytes, hex là 64 bytes
 
-def call_GenKey (keyFIle):
-    # convert python strings to bytes, as ctypes works with bytes
-    keyFIle = keyFIle.encode('utf-8')
-    # call the c function
-    GenerateAndSaveKey(keyFIle)
-    print("Random key generated and saved to", keyFIle.decode('utf-8'))
+    # Gọi hàm từ thư viện C++
+    HMAC_SHA3_256(key, data, output_hexmac, ctypes.sizeof(output_hexmac))
 
-if len(sys.argv) < 2:
-    print("Usage: path/python.exe "+ sys.argv[0]+" HMAC_SHA3_256 <KeyFile> <input_filename> <output_filename>")
-    print("Usage: path/python.exe "+ sys.argv[0]+" GenerateAndSaveKey <KeyFile>")
-    sys.exit(1)
-
-mode = sys.argv[1]
-if mode == "HMAC_SHA3_256":
-    if len(sys.argv) != 5:
-        print("Usage: "+ sys.argv[0] +" "+sys.argv[1] +" <KeyFile> <input_filename> <output_filename>" )
-        sys.exit(1)
-    keyPath = sys.argv[2]
-    inputPath = sys.argv[3]
-    signaturePath = sys.argv[4]
-    call_HMAC(keyPath,inputPath,signaturePath)
-    sys.exit(0)
-elif mode == "GenerateAndSaveKey":
-    if len(sys.argv) != 3:
-        print("Usage: "+ sys.argv[0] +" "+sys.argv[1] +" <KeyFile>" )
-        sys.exit(1)
-    keyPath = sys.argv[2]
-    call_GenKey(keyPath)
-    sys.exit(0)
-else:
-    print("Invalid option:")
-    print("Usage: path/python.exe "+ sys.argv[0]+" HMAC_SHA3_256 <KeyFile> <input_filename> <output_filename>")
-    print("Usage: path/python.exe "+ sys.argv[0]+" GenerateAndSaveKey <KeyFile>")
-    sys.exit(1)
+    return output_hexmac.value.decode('utf-8')
+# Ví dụ sử dụng hàm HMAC_SHA3_256
+# key = 'secret_key'
+# data = 'encrypted_data'
+# hexmac = hmac_sha3_256(key, data)
+# print(f'HMAC-SHA3-256: {hexmac}')
